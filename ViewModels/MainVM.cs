2a2d192e132964c7daedc01fb1e50cb858858570
Rin <rapora.run@gmail.com>
2022-05-01 20:57:37 +1000

    Добавьте файлы проекта.

---
+++ ViewModels/MainVM.cs
@@ -0,0 +1,18 @@
+﻿using Sanator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp15.Tools;
+using WpfApp15.ViewModels;
+
+namespace Sanator.ViewModels
+{
+    class MainVM : BaseVM
+    {
+
+    }
+
+
+}
